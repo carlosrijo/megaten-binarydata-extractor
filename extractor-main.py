3fa4d1802fce027cdd55a907a6d013c671f4eeda
Carlos Rijo <c.bate@campus.fct.unl.pt>
2020-09-01 22:57:26 +0100

    Fixed a wrong import

--- extractor-main.py
+++ extractor-main.py
@@ -1,6 +1,6 @@
 import sys, re
 from extractor import Extractor
-from MappingFormatsAndFiles import mapping_formats_and_files as faf
+from mapping_formats_and_files import formatsFiles as faf
 from config import base_client_binary_data_directory, base_output_directory_xml_or_tsv
 
 extractor = Extractor(base_client_binary_data_directory, base_output_directory_xml_or_tsv)
